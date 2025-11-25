19461e9c98f0fb8446591fe9d5842af493e6cf56
stephan <ardistephan@gmail.com>
2025-11-25 17:49:04 +0800

    Initial Commit

---
+++ HarryPotter/HarryPotterApp.swift
@@ -0,0 +1,17 @@
+//
+//  HarryPotterApp.swift
+//  HarryPotter
+//
+//  Created by stephan on 25/11/25.
+//
+
+import SwiftUI
+
+@main
+struct HarryPotterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HarryPotterTests/HarryPotterTests.swift
@@ -0,0 +1,17 @@
+//
+//  HarryPotterTests.swift
+//  HarryPotterTests
+//
+//  Created by stephan on 25/11/25.
+//
+
+import Testing
+@testable import HarryPotter
+
+struct HarryPotterTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
